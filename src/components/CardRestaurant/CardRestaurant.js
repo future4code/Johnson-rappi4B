fbff319e9be2ae7a3ae6b8981bb9b0be9c54d9db
andersonheidrich <andersonheidrich@hotmail.com>
2021-10-09 18:35:57 -0300

    estilização da tela home e search pré criado

---
+++ src/components/CardRestaurant/CardRestaurant.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { CardRestaurantContainer, LogoRestaurant, RestaurantName, DataRestaurant } from "./styled";
+
+const CardRestaurant = (props) => {
+    return (
+        // <Header />
+        // Search
+        // 
+        <CardRestaurantContainer>
+            <LogoRestaurant>
+                <img src={props.loja.logoUrl} alt="Logo do Restaurante"/>
+            </LogoRestaurant>
+            <RestaurantName>
+                <span>{props.loja.name}</span>
+            </RestaurantName>
+            <DataRestaurant>
+                <span>{props.loja.deliveryTime} min</span>
+                <span>Frete R${props.loja.shipping.toFixed(2).replace(".", ",")}</span>
+            </DataRestaurant>
+        </CardRestaurantContainer>
+        // <Footer />
+    )
+}
+
+export default CardRestaurant;
