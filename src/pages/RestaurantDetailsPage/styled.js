e82b8649f0975a7afbabc7471e45928d48ce7f35
juliosan-bit <julioesar.amancio@yahoo.com.br>
2021-10-21 23:40:49 -0300

    adicionado os botoes nos cards de detalhes e iniciando a logica de adicionar item

--- src/pages/RestaurantDetailsPage/styled.js
+++ src/pages/RestaurantDetailsPage/styled.js
@@ -1,88 +1,109 @@
 import styled from "styled-components";
 
 export const RestaurantDetailsPageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 360px;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const RestaurantContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    border-bottom: 1px solid #b8b8b8;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  border-bottom: 1px solid #b8b8b8;
+`;
 
 export const CardRestaurant = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 16px;
+  align-items: flex-start;
+  width: 90%;
+  color: #b8b8b8;
+  border-bottom: 1px solid black;
+
+  span {
+    margin-top: 12px;
+  }
+
+  div {
     display: flex;
-    flex-direction: column;
-    margin-top: 16px;
-    align-items: flex-start;
-    width: 90%;
-    color: #b8b8b8;
-    border-bottom: 1px solid black;
+    width: 60%;
+    justify-content: space-between;
+  }
 
-    span {
-        margin-top: 12px;
-    }
-    
-    div {
-        display: flex;
-        width: 60%;
-        justify-content: space-between;
-    }
+  .restaurant_name {
+    color: #de7e5d;
+  }
 
-    .restaurant_name {
-        color: #DE7E5D;
-    }
-
-    .restaurant_address {
-        margin-bottom: 12px;
-    }
-`
+  .restaurant_address {
+    margin-bottom: 12px;
+  }
+`;
 
 export const RestaurantImage = styled.img`
-    display: flex;
-    width: 100%;
-    border-radius: 8px 8px 0 0;       
-`
+  display: flex;
+  width: 100%;
+  border-radius: 8px 8px 0 0;
+`;
 
 export const ListProducts = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 96%;
-    padding: 4px;
-    margin-top: 8px;
-`
+  display: flex;
+  justify-content: center;
+  width: 96%;
+  padding: 4px;
+  margin-top: 8px;
+`;
 
 export const ProductCard = styled.div`
-    display: flex;
-    width: 328px;
-    height: 112px;
-    border: 1px solid #b8b8b8;
-    border-radius: 8px;
-    align-items: center;
-`
+  display: flex;
+  width: 328px;
+  height: 112px;
+  border: 1px solid #b8b8b8;
+  border-radius: 8px;
+  align-items: center;
+`;
 
 export const ProductImage = styled.img`
-    width: 97px;
-    height: 112px;
-    border-radius: 8px 0 0 8px;
-`
+  width: 97px;
+  height: 112px;
+  border-radius: 8px 0 0 8px;
+`;
 
 export const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 16px;
+
+  .product_name {
+    color: #de7e5d;
+  }
+
+  .product_description {
+    color: #b8b8b8;
+  }
+
+  .teste {
     display: flex;
-    flex-direction: column;
-    margin-left: 16px;
+    align-items: center;
+    width: 90%;
+    justify-content: space-between;
+  }
+`;
 
-    .product_name {
-        color: #DE7E5D;
-    }
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  
 
-    .product_description {
-        color: #b8b8b8;
-    }
-`
+  input {
+    margin: 0 auto;
+    width: 40%;
+    font-size: 18px;
+  }
+`;
