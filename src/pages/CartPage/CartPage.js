ac907ce39bf845f1fa2953d71d0902b69a5e9708
andrekalel <andregomesenator@gmail.com>
2021-10-23 00:27:26 -0300

    Implementação de lógica de adicionar quantidade ao produto

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -2,11 +2,11 @@ import React from "react";
 import { CartPageContainer } from "./styled";
 
 const CartPage = () => {
-    return (
-        <CartPageContainer>
-            <h1>CartPage</h1>
-        </CartPageContainer>
- )
-}
+  return (
+    <CartPageContainer>
+      <h1>CartPage</h1>
+    </CartPageContainer>
+  );
+};
 
 export default CartPage;
