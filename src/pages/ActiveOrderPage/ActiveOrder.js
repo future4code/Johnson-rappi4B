8f57eeb85f29c4c87d6b8f0ff3c03ff883db3d16
juliosan-bit <julioesar.amancio@yahoo.com.br>
2021-10-25 22:09:54 -0300

    finalizado o projeto labefoods

--- src/pages/ActiveOrderPage/ActiveOrder.js
+++ src/pages/ActiveOrderPage/ActiveOrder.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BASE_URL } from "../../constants/urls";
-import { ConatinerOrder } from "./styled";
+import { Buttons, ConatinerOrder, ContainerOrder2 } from "./styled";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { goToHistoryOrder } from "../../routes/coordinator";
 
 export const ActiveOrder = () => {
   const [dataOrder, setDataOrder] = useState();
+  const history = useHistory();
 
   useEffect(() => {
     axios
@@ -12,7 +16,6 @@ export const ActiveOrder = () => {
         headers: { auth: localStorage.getItem("token") },
       })
       .then((res) => {
-        console.log("ACTIVE ORDEM", res.data.order);
         setDataOrder(res.data.order);
       });
   }, []);
@@ -21,18 +24,40 @@ export const ActiveOrder = () => {
   const dateExpire = new Date(dataOrder && dataOrder.expiresAt);
 
   return (
-    <ConatinerOrder>
-      <h1>Seu(s) Pedido(s)</h1>
-      Valor Total: {dataOrder && dataOrder.totalPrice}
-      Restaurante: {dataOrder && dataOrder.restaurantName}
-      Data do Pedido:{" "}
-      {`${dateCreate.getDate()}/${
-        dateCreate.getMonth() + 1
-      }/${dateCreate.getFullYear()} ás ${dateCreate.getHours()}:${dateCreate.getMinutes()} `}
-      Data da Entrega:{" "}
-      {`${dateExpire.getDate()}/${
-        dateExpire.getMonth() + 1
-      }/${dateExpire.getFullYear()} ás ${dateExpire.getHours()}:${dateExpire.getMinutes()} `}
-    </ConatinerOrder>
+    <>
+      <ConatinerOrder>
+        <h1>Seu(s) Pedido(s)</h1>
+        <Buttons>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            size={"small"}
+            onClick={() => history.goBack()}
+          >
+            Voltar
+          </Button>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            size={"small"}
+            onClick={() => goToHistoryOrder(history)}
+          >
+            Historico
+          </Button>
+        </Buttons>
+        <ContainerOrder2>
+          <b>Restaurante:</b> {dataOrder && dataOrder.restaurantName}
+          <b>Valor Total:</b> R$ {dataOrder && dataOrder.totalPrice.toFixed(2).replace(".",",")}
+          <b>Data do Pedido:</b>
+          {`${dateCreate.getDate()}/${
+            dateCreate.getMonth() + 1
+          }/${dateCreate.getFullYear()} ás ${dateCreate.getHours()}:${dateCreate.getMinutes()} `}
+          <b>Data da Entrega:</b>
+          {`${dateExpire.getDate()}/${
+            dateExpire.getMonth() + 1
+          }/${dateExpire.getFullYear()} ás ${dateExpire.getHours()}:${dateExpire.getMinutes()} `}
+        </ContainerOrder2>
+      </ConatinerOrder>
+    </>
   );
 };
